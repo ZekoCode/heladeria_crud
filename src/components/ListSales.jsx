75b2ff9acd9ce91f5750d531e32a98f2f5586cb4
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2022-03-11 01:37:07 -0500

    Project with base code of form and components

---
+++ src/components/ListSales.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sale from './Sale'
+import './ListSales.css'
+
+function ListSales() {
+  return (
+    <div>
+      <Sale/>
+    </div>
+  )
+}
+
+export default ListSales
